d7ce19a0e2fdcd8719b0c9c044e4e68fe65a514c
KNikhith <nikhithkotluri@gmail.com>
2026-04-10 11:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	let strong=document.querySelectorAll("strong");
+	strong.forEach(word=>{
+		word.style.color=rgb(0,128,0);
+	});
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let strong=document.querySelectorAll("strong");
+	strong.forEach(word=>{
+		word.style.color=rgb(0,0,0);
+	})
     
 }
